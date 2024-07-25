07edf3283da453441d9249dc94035cae7598a8a3
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-07-25 12:55:28 +0000

    container config done

--- Basic_js/01_variables.js
+++ Basic_js/01_variables.js
@@ -1,1 +1,13 @@
-let name="hipash";
+const accountId=12233;
+let accountCity= "Jodhpur";
+accountState= "RJ";
+var accountPwd="636352";
+let accountDetail;
+
+/*
+Avoid using var because of block scope and fuction scope.
+*/
+
+console.table([accountId,accountCity,accountState,accountPwd,accountDetail]);
+
+
