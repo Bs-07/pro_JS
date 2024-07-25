d1b136b7a2d7d01bcded4ee7f7e0a009018a0cec
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-07-25 13:17:33 +0000

    container config done

---
+++ Basic_js/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let name="siomi";
+let age=28;
+let isLoggedIn= false;
+let state;
+let temp=null;
+
+//String => "hello"/'hello'
+//number => 2 to power 53
+//bigint 
+// boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique 
+
