36b2003b8427ba3267609f81d47d306e4133f214
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-07-25 13:48:28 +0000

    container config done

---
+++ Basic_js/03_conversionOperation.js
@@ -0,0 +1,47 @@
+let score ="33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let hello=score*1;
+console.log(hello);
+
+console.log(typeof null);
+/*  
+    typeOf(null) => object
+*/
+
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// Number conversion-----------------
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => undefined 
+// true/false => 1/0 
+// "33"*1 => 33 
+
+let isLoggedIn= "";
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// Boolean conversion----------------
+// 1=> true / 0=> false
+// "abc" => true
+// "" => false
+// null => false
+// undefine => false
+
+
+let value;
+
+let StringIt= String(value);
+console.log(StringIt);
+
+
+ 
+
