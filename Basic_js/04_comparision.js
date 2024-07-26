f22896138736121883527b7d8f2cca1032ec4931
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-07-26 10:00:59 +0000

    container config done

---
+++ Basic_js/04_comparision.js
@@ -0,0 +1,31 @@
+// Same DATATYPE Comparision----------
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// Different DATATYPE Comparision--------
+// Convert the string into num then compare:-
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+/*
+It just not check the value but also the datatype
+*/
+console.log("2" === 2);
+
+
+
+
+
