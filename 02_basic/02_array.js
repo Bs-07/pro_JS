b4b9611d796dbb47aaf9bc08b13bf97c3e4b5c4e
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-07-29 17:12:54 +0000

    container config done

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -24,6 +24,12 @@ console.log(Array.isArray("hello"));
 // from():
 console.log(Array.from('hello'));
 
+// Create an array based on a property of DOM Elements
+// const images = document.querySelectorAll("img");
+// const sources = Array.from(images, (image) => image.src);
+// const insecureSources = sources.filter((link) => link.startsWith("http://"));
+
+
 // of():
 const s1=100;
 const s2=200;
