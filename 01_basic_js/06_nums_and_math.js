0cd2ab6f6b4d216b57a870ce27ece95df97ef8ec
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-07-29 08:21:17 +0000

    container config done

--- 01_basic_js/06_nums_and_math.js
+++ 01_basic_js/06_nums_and_math.js
@@ -24,3 +24,27 @@ console.log(balance.toLocaleString('en-IN'));
 // =================Maths=============================================
 
 console.log(Math);
+console.log(Math.abs(-4)); // remove negative value
+console.log(Math.round(4.3)); //4
+console.log(Math.round(4.6)); //5
+console.log(Math.ceil(4.1)); //upper 5
+console.log(Math.floor(4.9)); //lower 4
+console.log(Math.pow(2,2)); //2**2
+
+console.log(Math.random()); //b/w 0 to 1
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+/*
+Math.random() = random b/w 0-1
+(max - min + 1) = give the range
+Math.floor() = avoid the decimal and get lower value
+add min value
+*/
+
+
+
+
