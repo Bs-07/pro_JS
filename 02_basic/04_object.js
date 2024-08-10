b956aaef3279af563736e58f4de0e1e8729d2474
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-10 05:30:44 +0000

    container config done

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -20,3 +20,38 @@ const regularUser= {
 
 console.log(regularUser.fullname?.userfullname.firstName);
 
+const obj1= {1:"a", 2:"b", 3:"c"};
+const obj2= {4:"d", 5:"e", 6:"f"};
+const obj3= {7:"g", 8:"h", 9:"i"};
+
+// const mergeObj= Object.assign({}, obj1,obj2,obj3);
+const mergeObj= {...obj1,...obj2};
+console.log(mergeObj);
+
+
+//data fatch from the database = array of object 
+const users= [
+    {
+        id:1,
+        email:"sd11@gmail.com",
+    },
+    {
+        id:2,
+        email:"ad11@gmail.com",
+    }
+];
+
+console.log(users[0].email);
+
+// get the jey or values in an array to use
+console.log(Object.keys(tinderUser)); 
+console.log(Object.values(tinderUser)); 
+
+// return array of array which contains keys and values
+console.log(Object.entries(tinderUser));
+
+// check the key if present or not, return true or false 
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('checkOrder'));
+
+
