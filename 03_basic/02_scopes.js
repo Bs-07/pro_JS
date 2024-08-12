39a97c72ff80b963d374c2c8093dca452d67fd06
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-12 13:25:16 +0000

    scope talk

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -7,7 +7,7 @@ const c=200;
     let b=10;
     const c=30
 }
-
+ 
 console.log(a,b,c);
 /*
 * variable inside a {} which is called block scope 
@@ -15,3 +15,30 @@ console.log(a,b,c);
 *
 * variable outside of block called global scope variable
 */
+
+function one() {
+    const username="john";
+
+    function two() {
+        const website="redit";
+        console.log(username);
+    }
+
+    // console.log(website);
+
+    two();
+}
+
+one();
+
+
+addOne(5);
+function addOne(num) {
+    return num+1;
+}
+
+
+// addTwo(6);
+const addTwo= function (num) {
+    return num+2;
+}   
