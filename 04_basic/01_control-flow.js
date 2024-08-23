9f4cc87734df05c7f39a35239911c86f076cd7a8
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-23 13:21:55 +0000

    if esle statement talks

--- 04_basic/01_control-flow.js
+++ 04_basic/01_control-flow.js
@@ -2,6 +2,20 @@
 
 // < , > , <= , >= , == , != , === , !==
 
-if(2!='2'){
-    console.log(`executed.`);
+// if(2!='2'){
+//     console.log(`executed.`);
+// }
+
+const balance= 1000;
+
+// if(balance!=1001) console.log(`you ara eligible for enroll.`);
+
+if(balance<500){
+    console.log(`much less`);
+} else if(balance>500 && balance <=800){
+    console.log(`ok ok`);       
+}else if(balance >800 && balance <=1000){
+    console.log(`good`);
+}else{
+    console.log(`excellent great`);
 }
