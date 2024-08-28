7e43ea8928990b0eb45bafdaba8f03c658a4549a
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-28 14:51:53 +0000

    switch case talk

---
+++ 04_control-flow/02_switch.js
@@ -0,0 +1,34 @@
+//* Switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=2;
+
+switch(month){
+    
+    case 1:
+        console.log(`It's January`);
+        break;
+    case 2:
+        console.log(`It's February `);
+        break;
+    case 3:
+        console.log(`It's March`);
+        break;
+    
+    default:
+        console.log(`enter valid data`);
+        break;
+}
+
+/*
+break is necessary because it stop the code to execute all those cases which are come dowwn the 
+correct case. (Except of Default)  
+*/
