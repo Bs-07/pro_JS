9ff8262f9d1e712f3af468cd5dd39fd528ee47e3
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-28 14:51:29 +0000

    if else talk

---
+++ 04_control-flow/01_if-else.js
@@ -0,0 +1,34 @@
+//  if
+
+// < , > , <= , >= , == , != , === , !==
+
+// if(2!='2'){
+//     console.log(`executed.`);
+// }
+
+const balance= 1000;
+
+// if(balance!=1001) console.log(`you ara eligible for enroll.`);
+
+// if(balance<500){
+//     console.log(`much less`);
+// } else if(balance>500 && balance <=800){
+//     console.log(`ok ok`);       
+// }else if(balance >800 && balance <=1000){
+//     console.log(`good`);
+// }else{
+//     console.log(`excellent great`);
+// }
+
+// const isLoggedIn=true;
+// const haveCreditCard=true;
+
+// if(isLoggedIn && haveCreditCard && 2==2){
+//     console.log(`allowed to purchase`);
+// }
+// if(userLoggedInGoogle || userLoggedInGitHub){
+//     console.log(`user logged In`);
+// }
+
+
+
