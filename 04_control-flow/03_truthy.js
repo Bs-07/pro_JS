7b485e20d1c45d4209a0018a2e9bf151eea6b43a
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-28 14:52:23 +0000

    truthy falsy value talk

---
+++ 04_control-flow/03_truthy.js
@@ -0,0 +1,11 @@
+const userEmail= "hello";
+
+if(userEmail){
+    console.log(`user have email`);
+}else{
+    console.log(`user doesn't have email`);
+}
+
+// Falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
