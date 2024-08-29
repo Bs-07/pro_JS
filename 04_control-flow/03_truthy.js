b33c1a2c84c368ac701cc99c3f2af58a3579fa18
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-08-29 14:09:09 +0000

    thuthy value talk

--- 04_control-flow/03_truthy.js
+++ 04_control-flow/03_truthy.js
@@ -1,4 +1,5 @@
-const userEmail= "hello";
+// const userEmail= "hello";
+const userEmail= [];
 
 if(userEmail){
     console.log(`user have email`);
@@ -9,3 +10,42 @@ if(userEmail){
 // Falsy values
 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy values
+
+// some String, " ", 'false' [], {}, function(){}
+
+// check array empty or not
+if (userEmail.length === 0) {
+    console.log(`array is empty.`);
+}
+
+// check object empty or not
+// Objct.keys(obj1) : return the new array of keys
+const emptyObj={};
+if (Object.keys(emptyObj).length === 0) {
+    console.log(`object is empty.`);
+}  
+
+
+//* Nullish Coalescing Operator (??): null undefined
+/*
+ It's used when get data from Database and to avoid unwanted situation 
+ so, 
+     before ?? : null / undefined 
+     after  ?? : data from DB / return 
+*/
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20 
+
+console.log(val1);
+
+//* Terniary Operator 
+ 
+// condition ? true : false
+const age=18;
+age>=18 ? console.log(`allowed`) : console.log(`not allowed `)
