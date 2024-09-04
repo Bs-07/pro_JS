e819913864790cde4a9cf2b35dd0eff719fc217e
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-09-04 17:14:38 +0000

    for loop talk

--- 05_iterations/01_loop.js
+++ 05_iterations/01_loop.js
@@ -1,6 +1,33 @@
 // for
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i*2);
 }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`loop: ${i}`);
+//     for (let j = 0; j < 5; j++) {
+//         console.log(`*`);
+//     }   
+// }
+
+// Break: stop the code
+// Continue: skip that condition start executing next lines
+ for (let index = 0; index < 10; index++) {
+    if(index==6){
+        break;
+    }    
+    console.log(index);
+ }
+
+//  for (let index = 0; index < 10; index++) {
+//     if(index==6)
+//         continue;
+//     console.log(index);
+//  }
+
