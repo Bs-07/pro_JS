feeceaefa4b7dbcf542d3ad6d81922ee560d85f2
Bhoopendra Singh <138092427+Bs-07@users.noreply.github.com>
2024-09-05 17:21:07 +0000

    while loop talk

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,23 @@
+// while loop
+
+// let index=0;
+// while (index<=10) {
+//     console.log(index);
+//     index++;
+// }
+
+const superHero = ['ironman', 'spiderman', 'captain America '];
+// console.log(superHero.length);
+let i=0;
+while(i<superHero.length){
+    console.log(`I'm ${superHero[i]}`);
+    i++;
+}
+
+// do while 
+
+
+// let index=0;
+// do {
+//     console.log(index);
+// } while (index<10);
